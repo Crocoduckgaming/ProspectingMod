9a634a3a7fd2ce9875759c4869e709d320ce405b
Jonathan Long <gamesinmyfood@gmail.com>
2017-08-23 13:25:51 -0400

    Mod wont show up in forge?

--- src/main/java/Croco/Prospecting/Prospecting.java
+++ src/main/java/Croco/Prospecting/Prospecting.java
@@ -3,15 +3,12 @@ package Croco.Prospecting;
 import Croco.Prospecting.Tab.CreativeTabProspecting;
 import Croco.Prospecting.proxy.CommonProxy;
 import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-
-import java.awt.*;
 
 @Mod(modid = Prospecting.MODID, version = Prospecting.VERSION, name = Prospecting.NAME)
 public class Prospecting {
@@ -20,9 +17,9 @@ public class Prospecting {
     public static final String NAME = "Prospecting";
 
     @SidedProxy(clientSide = "Croco.Prospecting.proxy.ClientProxy", serverSide = "Croco.Prospecting.proxy.CommonProxy")
-    public static CommonProxy proxy;
+        public static CommonProxy proxy;
     @Mod.Instance
-    public static Prospecting instance;
+        public static Prospecting instance;
 
     public static CreativeTabProspecting tabProspecting;
 
@@ -33,8 +30,8 @@ public class Prospecting {
     }
     @EventHandler
     public void init(FMLInitializationEvent event) {
-        tabProspecting = new CreativeTabProspecting(CreativeTabs.getNextID(), "tab_Prospecting")
-        proxy.Init(event);
+        tabProspecting = new CreativeTabProspecting(CreativeTabs.getNextID(), "tab_Prospecting");
+        proxy.init(event);
     }
     @EventHandler
     public void postInit(FMLPostInitializationEvent event){
